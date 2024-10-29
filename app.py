181a143dff35afe69744e5e152b7de25dbbb2460
Meo <soemoe@Meos.local>
2024-10-29 15:58:33 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Pdf Reader", page_icon=":books:")
+    st.header="A Place where you can ask anything about your Pdf"
+    st.text_input("Ask anything")
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Upload your desired Pdfs")
+        st.button("Process")
+
+if __name__== "main":
+    main()
+
